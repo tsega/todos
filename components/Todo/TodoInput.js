4f0902f14a83f49b2850bd3589e3dda178dede9f
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2020-09-23 17:09:13 +0300

    Mutation and update cache.

---
+++ components/Todo/TodoInput.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { GET_MY_TODOS } from './TodoPrivateList';
+
+const ADD_TODO = gql`
+  mutation($todo: String!, $isPublic: Boolean!) {
+    insert_todos(objects: { title: $todo, is_public: $isPublic }) {
+      affected_rows
+      returning {
+        id
+        title
+        created_at
+        is_completed
+      }
+    }
+  }
+`;
+
+const TodoInput = ({ isPublic = false }) => {
+  let input;
+
+  const [todoInput, setTodoInput] = useState('');
+
+  const updateCache = (cache, {data}) => {
+    // If this is for the public feed, do nothing
+    if(isPublic) {
+      return null;
+    }
+
+    // Fetch the todos from the cache
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS
+    });
+
+    // Add the new todo to the cache
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({
+      query: GET_MY_TODOS,
+      data: {todos: [newTodo, ...existingTodos.todos]}
+    });
+  };
+
+  const resetInput = () => {
+    setTodoInput(' ');
+  }
+
+  const [addTodo] = useMutation(ADD_TODO, {
+    update: updateCache,
+    onCompleted: resetInput
+  });
+
+  return (
+    <form
+      className="formInput"
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTodo({variables: {todo: todoInput, isPublic}})
+      }}
+    >
+      <input
+        className="input"
+        placeholder="What needs to be done?"
+        value={todoInput}
+        onChange={ e => (setTodoInput(e.target.value))}
+        ref={n => (input = n)}
+      />
+      <i className="inputMarker fa fa-angle-right" />
+    </form>
+  );
+};
+
+export default TodoInput;
---
+++ components/Todo/TodoPrivateList.js
@@ -0,0 +1,82 @@
+import React, { useState, Fragment } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+import TodoItem from './TodoItem';
+import TodoFilters from './TodoFilters';
+
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(
+      where: { is_public: { _eq: false } }
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      created_at
+      is_completed
+    }
+  }
+`;
+
+const TodoPrivateList = (props) => {
+  const [state, setState] = useState({
+    filter: 'all',
+    clearInProgress: false,
+  });
+
+  const filterResults = (filter) => {
+    setState({
+      ...state,
+      filter: filter,
+    });
+  };
+
+  const clearCompleted = () => {};
+
+  const { todos } = props;
+  let filteredTodos = todos;
+
+  if (state.filter === 'active') {
+    filteredTodos = todos.filter((todo) => todo.is_completed !== true);
+  } else if (state.filter === 'completed') {
+    filteredTodos = todos.filter((todo) => todo.is_completed === true);
+  }
+
+  const todoList = [];
+  filteredTodos.forEach((todo, index) => {
+    todoList.push(<TodoItem key={index} index={index} todo={todo} />);
+  });
+
+  return (
+    <Fragment>
+      <div className="todoListWrapper">
+        <ul>{todoList}</ul>
+      </div>
+
+      <TodoFilters
+        todos={filteredTodos}
+        currentFilter={state.filter}
+        filterResultsFn={filterResults}
+        clearCompletedFn={clearCompleted}
+        clearInProgress={state.clearInProgress}
+      />
+    </Fragment>
+  );
+};
+
+const TodoPrivateListQuery = () => {
+  const { loading, error, data } = useQuery(GET_MY_TODOS);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Error...</div>;
+  }
+  return <TodoPrivateList todos={data.todos} />;
+};
+
+export default TodoPrivateListQuery;
+export { GET_MY_TODOS };
